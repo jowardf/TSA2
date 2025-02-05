85460b875a4525ca2e19046e40285cce008c7ca5
jowardf <137141294+jowardf@users.noreply.github.com>
2025-02-05 13:59:15 +0800

    Initial code

---
+++ script.js
@@ -0,0 +1,21 @@
+function Mon() {
+	document.getElementById("box").style.backgroundColor="red";
+}
+function Tues() {
+	document.getElementById("box").style.backgroundColor="orange";
+}
+function Wed() {
+	document.getElementById("box").style.backgroundColor="yellow";
+}
+function Thurs() {
+	document.getElementById("box").style.backgroundColor="green";
+}
+function Fri() {
+	document.getElementById("box").style.backgroundColor="blue";
+}
+function Sat() {
+	document.getElementById("box").style.backgroundColor="indigo";
+}
+function Sun() {
+	document.getElementById("box").style.backgroundColor="violet";
+}
