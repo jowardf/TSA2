72f1d931969ea53fb418f6b80b157ff5e6c565fb
jowardf <jowardf2000@gmail.com>
2025-02-11 12:18:32 +0800

    style: code polishing

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
-function Mon() {
-	document.getElementById("box").style.backgroundColor="red";
-}
-function Tues() {
-	document.getElementById("box").style.backgroundColor="orange";
-}
-function Wed() {
-	document.getElementById("box").style.backgroundColor="yellow";
-}
-function Thurs() {
-	document.getElementById("box").style.backgroundColor="green";
-}
-function Fri() {
-	document.getElementById("box").style.backgroundColor="blue";
-}
-function Sat() {
-	document.getElementById("box").style.backgroundColor="indigo";
-}
-function Sun() {
-	document.getElementById("box").style.backgroundColor="violet";
+function showBox(color) {
+	let box = document.getElementById("box");
+	box.classList.remove("animate");
+	box.style.backgroundColor = color;
+	void box.offsetWidth; // Trigger reflow
+	box.classList.add("animate");
 }
